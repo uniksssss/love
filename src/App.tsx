c819b57ffb40ad511bcc52849fb44f965224ff35
Плюснина Ксения <plusnina@MacBook-Pro-Plusnina-2.local>
2026-02-14 01:09:59 +0500

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainScreen from "./pages/main-screen/main-screen";
 import NoScreen from "./pages/no-screen/no-screen";
 import YesScreen from "./pages/yes-screen/yes-screen";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainScreen />} />
         <Route path="yes" element={<YesScreen />} />
         <Route path="no" element={<NoScreen />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
